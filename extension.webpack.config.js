58da5eec7f668ce0acb5484fb3d980a1694bfb50
Joji Augustine <jojimail@gmail.com>
2020-10-08 22:05:24 +0530

    Add extension webpack config

---
+++ extension.webpack.config.js
@@ -0,0 +1,12 @@
+//@ts-check
+
+'use strict';
+
+const withDefaults = require('../shared.webpack.config');
+
+module.exports = withDefaults({
+	context: __dirname,
+	entry: {
+		extension: './src/extension.ts'
+	}
+});
